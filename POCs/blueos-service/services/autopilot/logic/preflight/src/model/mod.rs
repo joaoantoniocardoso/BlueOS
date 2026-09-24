4b15b0d0f97cc017f439a7bad5288bd85a5ebc30
João Antônio Cardoso <joao.maker@gmail.com>
2026-09-24 19:08:50 -0300

    POCs: Remove the blueos-service POC

--- POCs/blueos-service/services/autopilot/logic/preflight/src/model/mod.rs
+++
@@ -1,7 +0,0 @@
-mod command;
-mod sensor;
-mod snapshot;
-
-pub use command::{Command, Event, IoRequest};
-pub use sensor::Sensor;
-pub use snapshot::{Query, Snapshot, View};
--- POCs/blueos-service/services/calibration/logic/sensors/src/model/use_case.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum UseCase {
-    CalibrateGyroscope,
-    CalibrateBarometer,
-    CalibrateStationarySensors,
-    CancelCalibration,
-}
